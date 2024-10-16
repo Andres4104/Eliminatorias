65dfd13b61af45bade61ba2373f6b5302568d308
andres4104 <andreseste46810@gmail.com>
2024-10-16 09:12:36 -0500

    Primer commit con mi proyecto

--- album/views.py
+++ album/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from album.models import Team, Player
 
 # Create your views here.
+
+class TeamListView(ListView):
+    model = Team
+
+class PlayerListView(ListView):
+    model = Player
